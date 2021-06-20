5907ee31bbeccd8e42e38572ab84c0fa382308fb
Deepak <deepak.kiranagi@cgi.com>
2021-06-20 23:02:37 +0530

    first commit

---
+++ src/main/java/com/example/gitPush/GitPushApplication.java
@@ -0,0 +1,21 @@
+package com.example.gitPush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPushApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String getData() {
+		return "Hi Deepak";
+	}
+
+}
---
+++ src/test/java/com/example/gitPush/GitPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitPush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
